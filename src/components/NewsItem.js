bceabc438ab6da20fed607daa239dbfeaf34cb21
Dornala Ajay <ajaydornala1729@gmail.com>
2024-02-23 20:27:56 +0530

    Addiing Routes to NewsMonkey

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -3,16 +3,16 @@ import React, { Component } from 'react'
 export class NewsItem extends Component {
 
   render() {
-    let {title,description,imageUrl,newsUrl}=this.props;
+    let { title, description, imageUrl, newsUrl } = this.props;
     return (
       <div className='my-3'>
-        <div className="card" style={{width: "18rem"}}>
-          <img src={imageUrl} className="card-img-top" alt="..."/>
-            <div className="card-body">
-              <h5 className="card-title">{title}</h5>
-              <p className="card-text">{description}...</p>              
-              <a href={newsUrl} target='_blank' rel='noreferrer' className="btn btn-sm btn-primary">Read More</a>
-            </div>
+        <div className="card" >
+          <img src={!imageUrl ? "https://images.tijd.be/view?iid=dc:66572028&context=ONLINE&ratio=16/9&width=640&u=1464200700000" : imageUrl} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <a href={newsUrl} target='_blank' rel='noreferrer' className="btn btn-sm btn-dark">Read More</a>
+          </div>
         </div>
       </div>
     )
